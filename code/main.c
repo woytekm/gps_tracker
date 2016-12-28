463d031566648470a779a3f8671b552c5cdda892
woytekm <woytekm@gmail.com>
2016-12-28 22:48:10 +0100

    first commit

--- code/main.c
+++ code/main.c
@@ -1,15 +1,3 @@
-/* Copyright (c) 2014 Nordic Semiconductor. All Rights Reserved.
- *
- * The information contained herein is property of Nordic Semiconductor ASA.
- * Terms and conditions of usage are described in detail in NORDIC
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
- *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
- *
- */
-
 /** 
  * GPS Tracker application for GPS Tracker project.
  * (c)2016 woytekm
